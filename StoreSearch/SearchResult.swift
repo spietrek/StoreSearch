42f564580d7a6366234cf382cbee0ce686bbdf4b
spietrek <spietrek@gmail.com>
2015-11-10 09:47:07 -0500

    Add preliminary search bar and table view.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Stephen Pietrek on 11/10/15.
+//  Copyright © 2015 Stephen Pietrek. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
